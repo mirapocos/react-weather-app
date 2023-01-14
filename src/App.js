c310d89d535aa4923dd86844d80b33b5f7be62b3
mirapocos <112176776+mirapocos@users.noreply.github.com>
2023-01-14 18:58:06 +0000

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project is coded by Mira Pocos and is{" "}
+        <a
+          href="https://github.com/mirapocos/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
