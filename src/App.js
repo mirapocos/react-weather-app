d1a4de0bd70d8f15c8cbae13ae54c67de66f4efb
mirapocos <112176776+mirapocos@users.noreply.github.com>
2023-01-14 21:54:51 +0000

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project is coded by Mira Pocos and is{" "}
-        <a
-          href="https://github.com/mirapocos/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project is coded by Mira Pocos and is{" "}
+          <a
+            href="https://github.com/mirapocos/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
