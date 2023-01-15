e6bfdb5cf48584ebd6854f3d687eef127f64d153
mirapocos <112176776+mirapocos@users.noreply.github.com>
2023-01-15 00:00:25 +0000

    Added basic styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import React from "react";
+import Weather from "./Weather";
+
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
+        <Weather />
         <footer>
           This project is coded by Mira Pocos and is{" "}
           <a
